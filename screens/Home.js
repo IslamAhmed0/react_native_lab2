d6074bf4ac2df86b3080049a7ec4691685cf9bf6
IslamAhmed0 <ea43533@gmail.com>
2021-04-17 21:32:37 +0200

    change

--- screens/Home.js
+++ screens/Home.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 const Profile = ()=> {
     return(
         <View style={styles.container}>
-            <Text style={{ fontSize: 10,color: 'white'}}>
+            <Text style={{ fontSize: 30,color: 'white'}}>
                 my Profile :)
             </Text>
         </View>
